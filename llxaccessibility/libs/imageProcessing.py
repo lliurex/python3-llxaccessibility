1c3fd80fa47065d92953cd225f352b078a2b1b6d
lliurex <juanma1980@gmail.com>
2024-12-30 15:40:52 +0100

    WIP on OCR

--- llxaccessibility/libs/imageProcessing.py
+++ llxaccessibility/libs/imageProcessing.py
@@ -7,24 +7,37 @@ import subprocess
 import string
 import os
 import cv2
+import imutils
 import numpy as np
 from PIL import Image
 from PySide2.QtGui import QClipboard
+import speechd
+try:
+	from . import kconfig
+except:
+	import kconfig
 
 class filters():
 	def __init__(self,*args,**kwargs):
 		self.dbg=True
 
-	def opening(self,img):
+	def opening(self,image):
 		kernel = np.ones((5,5),np.uint8)
-		return cv2.morphologyEx(img, cv2.MORPH_OPEN, kernel)
+		return cv2.morphologyEx(image, cv2.MORPH_OPEN, kernel)
 
 	def thresholding(self,image):
-		return cv2.threshold(image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+		return(cv2.threshold(image, 0, 255,cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1])
+		#return cv2.threshold(image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
 
-	def sobel(self,img):
+	def distance(self,image):
+		image=cv2.distanceTransform(image,cv2.DIST_L2,5)
+		image=cv2.normalize(image, image, 0, 1.0, cv2.NORM_MINMAX)
+		image=(image * 255).astype("uint8")
+		return(image)
+
+	def sobel(self,image):
 		img = cv2.cvtColor(
-			src=img,
+			src=image,
 			code=cv2.COLOR_RGB2GRAY,
 		)
 
@@ -38,24 +51,40 @@ class filters():
 		)
 		return(img_sobel)
 		 
-	def morph(self,img):
-####	op = cv2.MORPH_OPEN
-####	img_morphology = cv2.morphologyEx(
-####		src=img,
-####		op=op,
-####		kernel=np.ones((5, 5), np.uint8),
-####	)
-		op = cv2.MORPH_CLOSE
-		img_morphology = cv2.morphologyEx(
-			src=img_morphology,
-			op=op,
-			kernel=np.ones((5, 5), np.uint8),
-		)
-		return(img_morphology)
+	def morph(self,image):
+		opening=cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (7, 7))
+		image=cv2.morphologyEx(image, cv2.MORPH_OPEN,opening)
+		return(image)
+
+	def getContours(self,image):
+		# find contours in the opening image, then initialize the list of
+		# contours which belong to actual characters that we will be OCR'ing
+		cnts=cv2.findContours(image.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+		cnts = imutils.grab_contours(cnts)
+		chars = []
+		# loop over the contours
+		for c in cnts:
+		# compute the bounding box of the contour
+			(x, y, w, h) = cv2.boundingRect(c)
+			# check if contour is at least 35px wide and 100px tall, and if
+			# so, consider the contour a digit
+			if w >= 5 and h >= 10:
+				chars.append(c)
+		# compute the convex hull of the characters
+		chars = np.vstack([chars[i] for i in range(0, len(chars))])
+		hull = cv2.convexHull(chars)
+		# allocate memory for the convex hull mask, draw the convex hull on
+		# the image, and then enlarge it via a dilation
+		mask = np.zeros(image.shape[:2], dtype="uint8")
+		cv2.drawContours(mask, [hull], -1, 255, -1)
+		mask = cv2.dilate(mask, None, iterations=2)
+		# take the bitwise of the opening image and the mask to reveal *just*
+		# the characters in the image
+		return(cv2.bitwise_and(image, image, mask=mask))
 	
 	# get grayscale image
 	def cvGrayscale(self,image):
-		return cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+		return(cv2.cvtColor(image, cv2.COLOR_RGB2GRAY))
 
 	#canny edge detection
 	def cvCanny(self,image):
@@ -92,6 +121,7 @@ class imageProcessing():
 		self.dbg=True
 		self.filter=filters()
 		self.clipboard=QClipboard()
+		self.kconfig=kconfig.kconfig()
 	#def __init__(self,parent=None):
 
 	def _debug(self,msg):
@@ -99,7 +129,7 @@ class imageProcessing():
 			print("imageProcessing: {}".format(msg))
 	#def _debug
 	
-	def getImageOCR(self,onlyClipboard=False,onlyScreen=False,lang="en"):
+	def getImageOCR(self,spellcheck=True,onlyClipboard=False,onlyScreen=False,lang="en"):
 		img=self._getImgForOCR(onlyClipboard,onlyScreen)
 		imgPIL=None
 		if os.path.isfile(img):
@@ -110,61 +140,80 @@ class imageProcessing():
 			except Exception as e:
 				print(e)
 		if imgPIL:
-			txt=self._readImg(imgPIL,lang=lang)
+			txt=self._readImg(imgPIL,lang=lang,spellcheck=spellcheck)
 		return(txt)
 	#def getImageOCR
 
 	def _getImgForOCR(self,onlyClipboard=False,onlyScreen=False):
+		cfg=self.kconfig.getTTSConfig()
 		outImg="/tmp/out.png"
 		img=None
 		if onlyScreen==False:
 			img=self.clipboard.image()
-		if img:
-			self._debug("Reading clipboard IMG")
-			img.save(outImg, "PNG")
-		else:
-			if onlyScreen==False:
-				img=self.clipboard.pixmap()
+			if img:
+				self._debug("Reading clipboard IMG")
+				img.save(outImg, "PNG")
+		elif onlyScreen==False:
+			img=self.clipboard.pixmap()
 			if img:
 				self._debug("Reading clipboard PXM")
 				img.save(outImg, "PNG")
-			elif onlyClipboard==False:
-				self._debug("Taking Screenshot")
+		elif onlyClipboard==False:
+			self._debug("Taking Screenshot")
+			if cfg.get("Screenshot",False)==False:
 				subprocess.run(["spectacle","-a","-e","-b","-c","-o",outImg])
+			else:
+				subprocess.run(["spectacle","-r","-e","-b","-c","-o",outImg])
 		return(outImg)
 	#def _getImgForOCR
 
 	def _processImg(self,img):
+		cfg=self.kconfig.getTTSConfig()
+		imgFilter=self.kconfig.getTextFromValueKScript("ocrwindow","filter",cfg.get("filter",0))
+		self._debug("Postprocessing img with {} filter".format(imgFilter))
 		outImg="{}".format(img)
 		image=cv2.imread(img,flags=cv2.IMREAD_COLOR)
-		h, w, c = image.shape
+		#h, w, c = image.shape
 		#self._debug(f'Image shape: {h}H x {w}W x {c}C')
-
-	#	image = image[:, :, 0]
+	#	image = image[:, :, 1]
+#		if "grayscale" in imgFilter.lower():
+#			image=self.filter.cvGrayscale(image)
+#		elif "sobel" in imgFilter.lower():
+#			image=self.filter.sobel(image)
+#		elif "threshold" in imgFilter.lower():
+#			image=self.filter.thresholding(image)
+#		elif "deskew" in imgFilter.lower():
+#			image=self.filter.cvDeskew(image)
+#		elif "opening" in imgFilter.lower():
+#			image=self.filter.opening(image)
+#		elif "smooth" in imgFilter.lower():
+#			image=self.filter.smooth(image)
+#		elif "canny" in imgFilter.lower():
+#			image=self.filter.cvCanny(image)
+		# From pyimage doc 
 		image=self.filter.cvGrayscale(image)
-	#	image=self.sobel(image)
-	#	image=self.thresholding(image)
-	#	image=self.cvDeskew(image)
-	#	image=self.opening(image)
-	#	image=self.smooth(image)
-	#	image=self.cvCanny(image)
+		image=self.filter.thresholding(image)
+		image=self.filter.distance(image)
+		image=self.filter.thresholding(image)
+		image=self.filter.morph(image)
+		image=self.filter.getContours(image)
+		# <- END
 		self._debug("Saving processed img as {}".format(outImg))
 		cv2.imwrite(outImg,image)
 		return(outImg)
 	#def _processImg
 
-	def _readImg(self,imgPIL,lang="en"):
+	def _readImg(self,imgPIL,lang="en",spellcheck=True):
 		txt=""
-		if lang=="en":
-			tsslang="eng"
-		elif lang=="es":
+		tsslang="eng"
+		if lang=="es":
 			tsslang="spa"
 		elif lang=="ca":
 			tsslang="cat"
 
 		imgPIL=imgPIL.convert('L').resize([5 * _ for _ in imgPIL.size], Image.BICUBIC)
 		imgPIL.save("/tmp/proc.png")
-		print("Reading with LANG {} - ".format(tsslang,lang))
+		self._debug("Reading with LANG {} - ".format(tsslang,lang))
 		with tesserocr.PyTessBaseAPI(lang=tsslang,psm=11) as api:
 			api.ReadConfigFile('digits')
 			# Consider having string with the white list chars in the config_file, for instance: "0123456789"
@@ -177,7 +226,8 @@ class imageProcessing():
 			txt=api.GetUTF8Text()
 			self._debug((api.AllWordConfidences()))
 		#txt=tesserocr.image_to_text(imgPIL,lang=tsslang)
-		txt=self._hunspellCheck(txt,lang)
+		if spellcheck==True:
+			txt=self._hunspellCheck(txt,lang)
 		return(txt)
 	#def _readImg
 
--- llxaccessibility/llxaccessibility.py
+++ llxaccessibility/llxaccessibility.py
@@ -4,7 +4,7 @@ import multiprocessing
 import json
 import llxaccessibility.libs.profileManager as profileManager
 import llxaccessibility.libs.ttsManager as ttsManager
-import llxaccessibility.libs.imageprocessing as imageprocessing
+import llxaccessibility.libs.imageProcessing as imageProcessing
 import llxaccessibility.libs.sddmManager as sddmManager
 import llxaccessibility.libs.kwinManager as kwinManager
 import llxaccessibility.libs.kconfig as kconfig
@@ -14,13 +14,14 @@ class client():
 	def __init__(self):
 		self.dbg=True
 		self.bus=None
-		app=QApplication(["tts"])
+		if QApplication.instance()==None:
+			app=QApplication(["tts"])
 		self.profile=profileManager.manager()
 		self.tts=ttsManager.manager()
 		self.sddm=sddmManager.manager()
 		self.kwin=kwinManager.manager()
 		self.kconfig=kconfig.kconfig()
-		self.imageprocessing=imageprocessing.imageprocessing()
+		self.imageProcessing=imageProcessing.imageProcessing()
 	#def __init__
 
 	def _debug(self,msg):
@@ -29,11 +30,11 @@ class client():
 	#def _debug
 
 	def getDockEnabled(self):
-		return(sef.profile.getDockEnabled())
+		return(self.profile.getDockEnabled())
 	#def getDockEnabled
 
 	def setDockEnabled(self,state):
-		return(sef.profile.setDockEnabled(state))
+		return(self.profile.setDockEnabled(state))
 	#def setDockEnabled
 	
 	def getGrubBeep(self):
@@ -55,7 +56,7 @@ class client():
 		return(self.kconfig.readKFile(*args,**kwargs))
 
 	def getKWinEffects(self):
-		return(self.kwin.getKwinEffects())
+		return(self.kwin.getKWinEffects())
 
 	def getKWinScripts(self):
 		return(self.kwin.getKWinScripts())
@@ -81,7 +82,7 @@ class client():
 		return(self.kwin.getClipboardText())
 
 	def getImageOcr(self,*args,**kwargs):
-		return(self.imageprocessing.getImageOCR(*args,**kwargs))
+		return(self.imageProcessing.getImageOCR(*args,**kwargs))
 
 	def _mpLaunchCmd(self,cmd):
 		proc=None
@@ -173,16 +174,20 @@ class client():
 	def readScreen(self,*args,onlyClipboard=False,onlyScreen=False):
 		txt=""
 		lang=self.readKFile("kwinrc","Script-ocrwindow","Voice")
+		clipboard=self.readKFile("kwinrc","Script-ocrwindow","Clipboard")
+		spellcheck=self.readKFile("kwinrc","Script-ocrwindow","Spellchecker")
 		scripts=self.kwin.getKWinScripts()
 		script=scripts.get("ocrwindow",{})
 		path=os.path.join("{}".format(os.path.dirname(script.get("path",""))),"contents","ui","config.ui")
 		lang=self.kconfig.getTextFromValueKScript(path,"Voice",lang)
 		langdict={"spanish":"es","valencian":"ca"}
 		lang=langdict.get(lang.lower(),"en")
-		if onlyScreen==False:
+		if clipboard==True:
 			txt=self.getClipboardText()
-		if not txt and onlyClipboard==False:
-			txt=self.getImageOcr(lang=lang)
+		if len(txt)==0:
+			if spellcheck==False:
+				lang=""
+			txt=self.getImageOcr(spellcheck=spellcheck,onlyScreen=not(clipboard),lang=lang)
 		if len(txt)>0:
 			self.tts.invokeReader(txt)
 	#def readScreen
