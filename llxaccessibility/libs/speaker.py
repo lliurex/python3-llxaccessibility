bac2297e13a18446c41530e518227552df76015c
lliurex <juanma1980@gmail.com>
2024-12-30 15:40:10 +0100

    Improve spd. Fix settings

--- llxaccessibility/libs/speaker.py
+++ llxaccessibility/libs/speaker.py
@@ -17,7 +17,6 @@ class speaker():
 		super().__init__()
 		self.dbg=True
 		self.txtFile=kwargs.get("txtFile")#.encode('iso8859-15',"replace")
-		self.spd=speechd.Client()
 		self.playing=False
 		self.mp=None
 		self.tmpFile=""
@@ -25,6 +24,7 @@ class speaker():
 		self.currentDate=kwargs.get("date","240101")
 		self.synth=kwargs.get("synth","")
 		self.kconfig=kconfig.kconfig()
+		self.spd=speechd.Speaker("accesshelper")
 		self.orca=None
 	#def __init__
 
@@ -53,23 +53,33 @@ class speaker():
 
 	def _runSpd(self,txt):
 		cfg=self.kconfig.getTTSConfig()
+		orcaConfig=cfg.get("orca",{}).get("profiles",{}).get("default",{}).get("voices",{}).get("default",{})
 		self.tmpFile=tempfile.mktemp(suffix=".mp3")
-		stretch=float(cfg.get("stretch",1))
-		rate=round(float(cfg.get("rate",0)),3)
-		#rate over 90
-		rate=(rate*80)/100
-		stretch=round(stretch-(rate/100),3)
+		stretch=int(cfg.get("stretch",1))
+		pitch=int(orcaConfig.get("average-pitch",1))
+		rate=int(orcaConfig.get("rate",cfg.get("rate",1)))
+		#rate over 80. Stretch deprecated 
+		stretch=int((stretch*100)-(rate*80/100)/100)
+		#spd=speechd.Speaker("accesshelper")
 		voiceLanguage=self.kconfig.getTextFromValueKScript("ocrwindow","voice",cfg.get("voice",0))
 		(voiceLocale,voice)=self._getSpdVoiceForLanguage(voiceLanguage)
-		mp=self.recordPulseStart()
+		voices=orcaConfig.get("family",{})
+		voiceLocale=voices.get("lang",voiceLocale)
+		voice=voices.get("name",voice)
+		if self.spd!=None:
+			self.spd==None
+		self.spd=speechd.Speaker("accesshelper")
 		self.spd.set_language(voiceLocale)
-		self.spd.set_voice(voice)
-		time.sleep(1)
+		self.spd.set_synthesis_voice(voice)
+		self.spd.set_rate(rate)
+		self.spd.set_pitch(pitch)
+		self._debug("Voice: {} VoiceLocale: {} Rate: {} Pitch: {}".format(voice,voiceLocale,rate,pitch))
+		mp=self.recordPulseStart()
 		self.spd.speak(txt,self.recordPulseEnd)
-	#def _runSay
+	#def _runSpd
 
 	def _getSpdVoiceForLanguage(self,lang):
-		voice="grandpa"
+		voice="female1"
 		voiceLocale="en"
 		if lang.lower()=="valencian":
 			lang="catalan"
@@ -77,7 +87,7 @@ class speaker():
 		elif lang.lower()=="spanish":
 			voiceLocale="es"
 		for spdlang in self.spd.list_synthesis_voices():
-			if spdlang[0].strip().lower().startswith(lang) and "male" in spdlang[-1].lower():
+			if spdlang[0].strip().lower().startswith(lang.lower()) and "male" in spdlang[-1].lower():
 				voiceLocale=spdlang[1]
 				voice=spdlang[-1]
 				break
@@ -85,17 +95,16 @@ class speaker():
 	#def _getSpdVoiceForLanguage
 
 	def recordPulseStart(self):
+		self._debug("START Pulse recording")
 		self.mp=multiprocessing.Process(target=self._capturePulseOutput,args=(self.tmpFile,),daemon=False)
 		self.mp.start()
 		#self._capturePulseOutput(outF)
 	#def recordPulseStart
 
 	def recordPulseEnd(self,*args):
-		if self.playing==False:
-			self.playing=True
+		if args[0]=="begin":
 			return
 		time.sleep(1)
-		self.playing=False
 		confDir=os.path.join(os.environ.get('HOME','/tmp'),".local/share/accesswizard/records")
 		mp3Dir=os.path.join(confDir,"mp3")
 		mp3File=os.path.join(mp3Dir,"{}.mp3".format(self.currentDate))
@@ -104,7 +113,6 @@ class speaker():
 		subprocess.run(cmd)
 		shutil.move(self.tmpFile,mp3File)
 		self._debug("PA: {}".format(mp3File))
-		print(self.mp)
 	#def recordPulseEnd
 
 	def _runFestival(self,txt):
@@ -171,8 +179,7 @@ class speaker():
 			cmd=["pacmd","list-sinks"]
 			proc=subprocess.run(cmd,encoding="utf8",stderr=subprocess.PIPE,stdout=subprocess.PIPE)
 			read=False
-			print(proc)
-			print("Reading sinks")
+			self._debug("Reading sinks")
 			for lproc in proc.stdout.split("\n"):
 				if lproc.strip().startswith("* index"):
 					read=True
@@ -184,15 +191,10 @@ class speaker():
 						break
 					self._debug("Default output: {}".format(defOutput))
 			cmd=["ffmpeg","-f","pulse","-ac","2","-i","{}.monitor".format(defOutput),outF]
-			print("RUN {}".format(cmd))
+			self._debug("RUN {}".format(cmd))
 			subprocess.run(cmd)
 		except Exception as e:
-			print("*****************")
-			print("*****************")
-			print(e)
-			print("*****************")
-			print("*****************")
+			self._debug("** Capture error: {}".format(e))
 		return(defOutput)
 	#def _capturePulseAudio
-
 #class speaker
